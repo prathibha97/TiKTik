fff61a5a92a3c74156e99f99bc86944f91b5a70c
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-18 14:00:23 +0530

    Bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2'>
       <Link href={'/'}>
       <div className='w-[100px] md:w-[130px]'>
-        <Image className='cursor-pointer' src={Logo} alt='tik-tak' layout='responsive'></Image>
+        <Image className='cursor-pointer' src={Logo} alt='tik-tak'></Image>
         </div>
         </Link>
     </div>
