abaa1f2cec91c8dda68d968962de98d1eb0f9315
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-18 20:46:55 +0530

    Add google login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import {GoogleLogin, GoogleLogout} from 'react-google-login'
-import {AiOutlineLogout} from 'react-icons/ai'
-import {BiSearch} from 'react-icons/bi'
-import {IoMdAdd} from 'react-icons/io'
-import Logo from '../utils/tiktik-logo.png'
+import { GoogleLogin } from '@react-oauth/google';
+import Image from 'next/image';
+import Link from 'next/link';
+import { createOrGetUser } from '../utils';
+import Logo from '../utils/tiktik-logo.png';
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2'>
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2">
       <Link href={'/'}>
-      <div className='w-[100px] md:w-[130px]'>
-        <Image className='cursor-pointer' src={Logo} alt='tik-tak'></Image>
+        <div className="w-[100px] md:w-[130px]">
+          <Image className="cursor-pointer" src={Logo} alt="tik-tak"></Image>
         </div>
-        </Link>
-    </div>
-  )
-}
+      </Link>
+      <div>SEARCH</div>
 
-export default Navbar
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log('err')}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
