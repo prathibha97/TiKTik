b8d8d81079abd6145d4c0f3469ab54239dd52ac1
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-20 19:44:39 +0530

    Add Suggested Accounts component

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,10 +1,18 @@
+import axios from 'axios';
 import create from 'zustand';
 import { persist } from 'zustand/middleware';
+import { BASE_URL } from '../utils';
 
 const authStore = (set: any) => ({
   userProfile: null,
+  allUsers: [],
+
   addUser: (user: any) => set({ userProfile: user }),
   removeUser: () => set({ userProfile: null }),
+  fetchAllUsers: async () => {
+    const {data} = await axios.get(`${BASE_URL}/api/users`);
+    set({ allUsers: data });
+  },
 });
 
 const useAuthStore = create(
