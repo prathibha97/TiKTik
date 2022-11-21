9610e59ec53fe115fc139062d8b13fc5130b21ea
Prathibha Ratnayake <prsthibha@gmail.com>
2022-11-21 10:16:38 +0530

    Add Search page and functionality

---
+++ pages/api/profile/[id].ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { client } from '../../../utils/client';
+import {
+  singleUserQuery,
+  userCreatedPostsQuery,
+  userLikedPostsQuery,
+} from '../../../utils/queries';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const { id }: any = req.query;
+    const query = singleUserQuery(id);
+    const userVideosQuery = userCreatedPostsQuery(id);
+    const userLikedPosts = userLikedPostsQuery(id);
+
+    const user = await client.fetch(query);
+    const userVideos = client.fetch(userVideosQuery);
+    const userLikedVideos = client.fetch(userLikedPosts);
+
+    res.status(200).json({
+      user: user[0],
+      userVideos,
+      userLikedVideos,
+    });
+  }
+}
